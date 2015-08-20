446cc06ea92d59b1cbbe5b8afa2cd2e1ba635760
leksw <aleks.woronow@yandex.ru>
2015-08-20 18:35:17 +0000

    t5 fixed

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -31,11 +31,12 @@ def request_view(request):
 @not_record_request
 def request_ajax(request):
     if request.is_ajax():
-        request_list = RequestStore.objects.filter(new_request=1)
-        print request_list
-        r = serializers.serialize("json", request_list)
-        data = json.dumps(r)
-
+        new_request = RequestStore.objects.filter(new_request=1).count()
+        request_list = RequestStore.objects.all()[:10]
+        list = serializers.serialize("json", request_list) 
+        r = [{'list': list, 'num': new_request}]
+        data = json.dumps((new_request, list))
+        print data
         return HttpResponse(data, content_type="application/json")
 
     return None
--- assets/js/request.js
+++ assets/js/request.js
@@ -1,30 +1,25 @@
 var helloRequest = (function($){
 
   function handleRequest(data) {
+     console.log(data);
     var items = ['<thead><tr><th>Path</th>\
                  <th>Method</th><tr></thead>'];
-    var j_data = $.parseJSON(data);
-    var id = 0;
+                 
+    var j_data = $.parseJSON(data[1]);
+    var id = data[0];
     $.each(j_data, function(i, val) {
-      id += parseInt(val.fields.new_request, 10);
-      if (i < 10 ){
-          if (parseInt(val.fields.new_request, 10) != 0){
-               items.push('<tr id="' + val.pk + '">'
-                          + '<td><strong>' + val.fields.path + '</strong></td>'
-                          + '<td><strong>'+ val.fields.method + '</strong></td>'
-                          + '</tr>');
-          }else{
-              items.push('<tr id="' + val.pk + '">'
-                   + '<td>' + val.fields.path + '</td>'
-                   + '<td>'+ val.fields.method + '</td>'
-                   + '</tr>');
-          }
-    }
+        items.push('<tr>'
+                    + '<td>' + val.fields.path + '</td>'
+                    + '<td>' + val.fields.method + '</td>'
+                    + '</tr>'
+        );
    });
    var title = $('title').text().split('-')[1] || $('title').text();
    var pre_titile = id ? id + '-' : '';
    $('#request').html(items);
    $('title').text(pre_titile + title);
+   var str_elem = parseInt(id) + 2;
+   $('tr:lt('+str_elem+')').not('tr th').addClass('req');
  }
 
  return {
